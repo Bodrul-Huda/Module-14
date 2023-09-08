56c08171d4c6983b9a8a5c9c64d0dfa4bc7c7800
Bodrul Huda <121507164+Bodrul-Huda@users.noreply.github.com>
2023-09-08 21:36:32 +0800

    route.js

--- src/app/api/email/route.js
+++ src/app/api/email/route.js
@@ -2,15 +2,14 @@ import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 import smtpTransport from "nodemailer-smtp-transport";
 
-export async function POST(req, res) {
+export async function POST(req) {
+  const { subject, message } = await req.json();
+
   const { searchParams } = new URL(req.url);
 
-  let ToEmail = searchParams.get("email");
-
-  // Set up Transporter
-
+  let email = searchParams.get("email");
   try {
-    let Transporter = nodemailer.createTransport(
+    let transporter = nodemailer.createTransport(
       smtpTransport({
         host: "mail.teamrabbil.com",
         port: 25,
@@ -23,18 +22,28 @@ export async function POST(req, res) {
       })
     );
 
-    // Prepare Email
-
-    let myEmail = {
-      from: "Test email from Next js application <info@teamrabbil.com",
-      to: ToEmail,
-      subject: " Test Email",
-      Text: "Test email from Next js application",
+    const mailOption = {
+      from: "info@teamrabbil.com",
+      to: email,
+      subject: "Send Email Tutorial",
+      text: "Send Email Tutorial",
+      html: `
+        <h3>Hello There</h3>
+        <li> title: ${subject}</li>
+        <li> message: ${message}</li>
+        `,
     };
 
-    let result = await Transporter.sendMail(myEmail);
-    return NextResponse.json({ msg: result });
-  } catch (e) {
-    return NextResponse.json({ msg: "Failed" });
+    await transporter.sendMail(mailOption);
+
+    return NextResponse.json(
+      { message: "Email Sent Successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Failed to Send Email" },
+      { status: 500 }
+    );
   }
 }
